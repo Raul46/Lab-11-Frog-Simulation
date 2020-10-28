4d9eff04cf55937babb8d9df6d7e176c5ef526df
Dave Avis <charles.avis@springbranchisd.com>
2020-10-28 12:05:53 -0500

    Updated hopDistance to include negative results

--- FrogSimulation.java
+++ FrogSimulation.java
@@ -28,7 +28,7 @@ public class FrogSimulation
     private int hopDistance()
     {
         int max = 10;
-        int min = 1;
+        int min = -10;
         return (int)(Math.random() * ((max - min) + 1)) + min;
     }
     
